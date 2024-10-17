86ce8da48b9a67ea4be536cde39a0263a4b1c90b
mingmong <mingmong@gmail.com>
2024-10-17 21:38:19 +0900

    first commit

---
+++ src/main/java/com/refurbmarket/RefurbMarketApplication.java
@@ -0,0 +1,13 @@
+package com.refurbmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefurbMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefurbMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/refurbmarket/RefurbMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refurbmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefurbMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
